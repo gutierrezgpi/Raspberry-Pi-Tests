1d6bc097aefb7ee1ac0d913e72da0f3fca883657
Gutierrez <gutierrez.asd@hotmail.com>
2025-03-09 00:38:23 -0300

    Atualizado serial receive

--- Pico/Serial/serial_receive.py
+++ Pico/Serial/serial_receive.py
@@ -4,11 +4,11 @@ try:
     # Porta Windows
     #porta_serial = serial.Serial('COM4', 9600)
     # Porta Linux
-    porta_serial = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    porta_serial = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
     while True:
         try:
-            dados = porta_serial.readline().decode('utf-8').strip()
+            dados = porta_serial.readline().decode('utf-8', errors='ignore').strip()
             if dados:
                 print(dados)
         except KeyboardInterrupt:
