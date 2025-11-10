51a0c0218694eab8b03b72d8c7c1d7da426a8bac
Gutierrez <gutierrez.asd@hotmail.com>
2025-11-10 17:06:45 -0300

    Padronizando diretorios

--- Pico/time_now.py
+++ Pico/time_now.py
@@ -13,6 +13,7 @@ time_now = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(year, month, day, 
 print(f"Formatted output: {time_now}")
 
 '''
+
 Output:
 
 Controller output: struct_time(tm_year=2024, tm_mon=9, tm_mday=27, tm_hour=21, tm_min=21, tm_sec=34, tm_wday=4, tm_yday=271, tm_isdst=-1)
